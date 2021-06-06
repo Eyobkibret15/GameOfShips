9fd1a135a726b4fd9a1a999635c6b2aba6c8010c
eyob <ayaleweyob15@gmail.com>
2021-06-06 02:34:50 +0200

    display the winner input

--- GameOfShips/Program.cs
+++ GameOfShips/Program.cs
@@ -6,32 +6,113 @@ namespace GameOfShips
     {
         static protected string[,] my_bord = new string[11, 11];
         static protected string[,] opponent_bord = new string[11, 11];
+        static protected string[,] demo_opponent_bord = new string[11, 11];
 
         static void Main(string[] args)
         {
             Console.WriteLine("=========================================WELCOME TO GAME OF SHIPS=======================");
             Console.WriteLine();
             Console.WriteLine();
-
-            opponent_bord = InitializingTheBord(opponent_bord);
             opponent_bord = opponent_master_ships(opponent_bord);
+            demo_opponent_bord = InitializingTheBord(demo_opponent_bord);
 
             Console.WriteLine("do you want to see opponent bord y/n ?");
             string choice = Console.ReadLine().ToLower();
-
-                if (choice == "y" || choice == "yes")
+            if (choice == "y" || choice == "yes")
             {
                 displaying_opponent_bord();
             }
-
             my_bord = InitializingTheBord(my_bord);
             my_bord = User_master_ships(my_bord);
-            
-
-            
-
             displaying_user_bord();
+            StartTheGame();
             
+
+        }
+
+        static void StartTheGame()
+        {
+           displaying_opponent_bord();
+           int Turn_Count = 0;
+           int total_enemy_damage = 0;
+           while(true)
+           {
+                Console.WriteLine("choose the row and column cordinates for firing  from 1 to 10   respectively ? ");
+                int fire_row = Convert.ToInt32(Console.ReadLine());
+                int fire_column = Convert.ToInt32(Console.ReadLine());
+                if (fire_row < 1 || fire_row > 10 || fire_column < 1 || fire_column > 10)
+                {
+                    Console.WriteLine("please enter the proper cells , out of bound \n Try again");
+                    continue;
+                }
+                else if (opponent_bord[fire_row , fire_column] == "X")
+                {
+                    Console.WriteLine("please enter the proper cells, already damaged cell \n Try again");
+                    continue;
+                }
+                else if (opponent_bord[fire_row, fire_column] == "8")
+                {
+                    Console.WriteLine("you damage one cell\n , fire again");
+                    demo_opponent_bord[fire_row, fire_column] = "X";
+                    opponent_bord[fire_row, fire_column] = "X";
+                    Turn_Count += 1;
+                    total_enemy_damage += 1;
+                    if (total_enemy_damage > 19)
+                    {
+                        Console.WriteLine("CONGRATULATION YOU WIN THE GAME");
+                        Environment.Exit(1);
+                    }
+                }
+                else if (demo_opponent_bord[fire_row, fire_column] == "." )
+                {
+                    Console.WriteLine("you miss the target \n , opponent's turn");
+                    Turn_Count += 1;
+                    opponents_fire();
+
+                }
+                else
+                {
+                    Console.WriteLine("222222");
+                    continue;
+                }
+            }
+
+        }
+
+        static void opponents_fire()
+        {
+            Random rand = new Random();
+            int total_user_damage = 0;
+            while (true)
+            {
+                int fire_row = rand.Next(1,11);
+                int fire_column = rand.Next(1,11); 
+                if (my_bord[fire_row, fire_column] == "X")
+                {
+                    continue;
+                }
+                else if (my_bord[fire_row, fire_column] == "8")
+                {
+                    total_user_damage += 1;
+                    if (total_user_damage > 19)
+                    {
+                        Console.WriteLine("YOU LOSE THE GAME \n PLAY NEW GAME");
+                        Environment.Exit(1);
+                    }
+                    my_bord[fire_row, fire_column] = "X";
+                    continue;
+                }
+                else if (my_bord[fire_row, fire_column] == ".")
+                {
+                    StartTheGame();
+                }
+
+                else
+                {
+                    Console.WriteLine("0000");
+                    continue;
+                }
+            }
         }
         static void displaying_user_bord()
         {
@@ -58,7 +139,7 @@ namespace GameOfShips
             {
                 for (int j = 0; j <= 10; j++)
                 {
-                    Console.Write(String.Format("{0}\t", InitializingTheBord(opponent_bord)[i, j]));
+                    Console.Write(String.Format("{0}\t", opponent_bord[i, j]));
 
                 }
                 Console.WriteLine();
@@ -464,7 +545,7 @@ namespace GameOfShips
                 starting_row = random.Next(1, 11);
                 starting_column = random.Next(1, 11);
 
-                if(bord[starting_row,starting_column]=="8")
+                if(bord[starting_row,starting_column] =="8")
                 {
         
                     continue;
